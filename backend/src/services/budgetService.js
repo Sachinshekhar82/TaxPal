91e52f01f04ef1aba5e1d18470ac408f915b988e
Sachinshekhar82 <sachinshekhar7550@gmail.com>
2026-08-05 15:39:12 +0530

    Clean budgetService.js conflict markers

--- backend/src/services/budgetService.js
+++ backend/src/services/budgetService.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-const Budget = require("../models/Budget");
-
-// Create a new budget
-const createBudget = async (budgetData) => {
-  const budget = new Budget(budgetData);
-  return await budget.save();
-};
-
-// Get all budgets
-const getAllBudgets = async () => {
-  return await Budget.find();
-};
-
-// Get budget by ID
-const getBudgetById = async (id) => {
-  return await Budget.findById(id);
-};
-
-// Update budget
-const updateBudget = async (id, budgetData) => {
-  return await Budget.findByIdAndUpdate(id, budgetData, {
-    new: true,
-    runValidators: true,
-  });
-};
-
-// Delete budget
-const deleteBudget = async (id) => {
-  return await Budget.findByIdAndDelete(id);
-};
-
-module.exports = {
-  createBudget,
-  getAllBudgets,
-  getBudgetById,
-  updateBudget,
-  deleteBudget,
-};
-=======
 const mongoose = require("mongoose");
 const Budget = require("../models/Budget");
 const Transaction = require("../models/Transaction");
@@ -182,4 +142,3 @@ module.exports = {
   getBudgetProgress,
   getBudgetProgressFormatted 
 };
->>>>>>> main
