cdd69930fcd6eadc8a748a25fa33444aa9c5eca2
Sachinshekhar82 <sachinshekhar7550@gmail.com>
2026-08-05 15:38:48 +0530

    Fix conflict marker in budgetRoutes.js

--- backend/src/routes/budgetRoutes.js
+++ backend/src/routes/budgetRoutes.js
@@ -1,30 +1,7 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-
-const budgetController = require("../controllers/budgetController");
-const { validateBudget } = require("../validators/budgetValidator");
-
-// Create Budget
-router.post("/", validateBudget, budgetController.createBudget);
-
-// Get All Budgets
-router.get("/", budgetController.getAllBudgets);
-
-// Get Budget By ID
-router.get("/:id", budgetController.getBudgetById);
-
-// Update Budget
-router.put("/:id", validateBudget, budgetController.updateBudget);
-
-// Delete Budget
-router.delete("/:id", budgetController.deleteBudget);
-
-module.exports = router;
-=======
 const budgetController = require("../controllers/budgetController");
 const { protect } = require("../middleware/authMiddleware");
-
 const { budgetValidation, validate } = require("../validators/budgetValidator");
 
 router.use(protect);
@@ -37,4 +14,3 @@ router.put("/:id", budgetValidation, validate, budgetController.updateBudget);
 router.delete("/:id", budgetController.deleteBudget);
 
 module.exports = router;
->>>>>>> main
